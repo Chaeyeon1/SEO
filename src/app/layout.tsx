9424189f6935f7d95f71286a9f39534674ded7f9
chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2024-06-15 14:17:13 +0900

    feaet : google, naver verification 코드 next.js화하기

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,12 @@ export const metadata: Metadata = {
   title: 'Chaeyeon_SEO',
   description: '김수돌과 신채돌의 개발 일기',
   keywords: ['김수돌', '신채돌', 'SEO', '개발', '일기'],
+  verification: {
+    google: 'Ot9R1wLIt3DP15IpoikrTHTptAcVxD5f-MX5poEjgrQ',
+    other: {
+      'naver-site-verification': 'Ot9R1wLIt3DP15IpoikrTHTptAcVxD5f-MX5poEjgrQ',
+    },
+  },
 };
 
 export default function RootLayout({
@@ -17,14 +23,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <meta
-        name='google-site-verification'
-        content='Ot9R1wLIt3DP15IpoikrTHTptAcVxD5f-MX5poEjgrQ'
-      />
-      <meta
-        name='naver-site-verification'
-        content='f92dce300102fa2c730313dc1f2fc4c907edcc93'
-      />
       <body className={inter.className}>{children}</body>
     </html>
   );
